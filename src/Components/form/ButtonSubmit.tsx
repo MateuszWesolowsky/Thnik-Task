c41306c62dd245bffe21e30ae3a15520bdaec979
Mateusz <m.wesolowski.92@gmail.com>
2025-04-15 21:35:44 +0200

    Create button submit component

---
+++ src/Components/form/ButtonSubmit.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  isDisabled: boolean;
+}
+export const ButtonSubmit = ({ children, isDisabled }: Props) => {
+  return (
+    <button
+      type="submit"
+      disabled={isDisabled}
+      className={`text-white px-4 py-2 rounded-md w-full transition-colors ${
+        isDisabled
+          ? "bg-default cursor-not-allowed"
+          : "bg-active hover:bg-hover cursor-pointer"
+      }`}
+    >
+      {children}
+    </button>
+  );
+};
