68510161519d2f72943cc71852152908d8eb084c
Mateusz <m.wesolowski.92@gmail.com>
2025-04-15 21:40:29 +0200

    Create validate function

---
+++ src/utils/validateForm.ts
@@ -0,0 +1,14 @@
+import { FormErrors, FormValues } from "../types/types";
+
+export const validateForm = (formData: FormValues): FormErrors => {
+  const errors: FormErrors = {};
+
+  if (!formData.firstName.trim()) errors.firstName = "First name is required.";
+  if (!formData.lastName.trim()) errors.lastName = "Last name is required.";
+  if (!formData.email.trim()) errors.email = "Email is required.";
+  if (!formData.email.includes("@"))
+    errors.email = "Please use correct formatting. Example: address@email.com";
+  if (formData.photo === null) errors.photo = "Photo is required.";
+
+  return errors;
+};
